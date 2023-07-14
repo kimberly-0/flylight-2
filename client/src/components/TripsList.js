87b0ce19f6772ae3975e3afdcfecf8db1413f533
Kimberly Dijkmans <kim.14420@outlook.com>
2023-07-14 17:48:46 +0200

    created new Trips list component

--- client/src/components/TripsList.js
+++ client/src/components/TripsList.js
@@ -1,36 +1,33 @@
 import { useAsync } from '../hooks/useAsync'
-import { Link } from 'react-router-dom'
 import { getTrips } from '../services/userTrips'
 import TripPreview from './TripPreview'
+import { ReactComponent as Arrow } from '../assets/Arrow.svg'
 
-export default function TripList({ userId }) {
+export default function TripsList({ userId, shortList }) {
     const { loading, error, value: trips } = useAsync(() => getTrips({ userId }), [userId])
 
     if (loading) return <h1>Loading</h1>
 
     if (error) return <h1 className="error-msg">{error}</h1>
 
-    // console.log("Trips: " + JSON.stringify(trips))
+    const numOfListItems = shortList ? 2 : trips.length
 
     return (
         <>
             {trips?.length > 0 ? (
                 <div className='trip-list'>
-                    {trips.map(trip => {
-                        return (
-
-                            <div key={trip.id}>
-                                <TripPreview trip={trip} />
-                            </div>
-                        )
-                    })}
+                    {trips.slice(0, numOfListItems).map(trip => 
+                        <div key={trip.id}>
+                            <TripPreview trip={trip} />
+                        </div>
+                    )}
                 </div>
             ) : (
                 <div className="my-trips__items__empty">
                     <p>You don't have any trips yet, <br/>
                     click here to add a new trip!</p>
                     <div className="my-trips__items__empty__arrow-container">
-                    {/* <Arrow /> */}
+                    <Arrow />
                     </div>
                 </div>
             )}
