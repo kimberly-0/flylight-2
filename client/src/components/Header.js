3cd3a7bb3c695f5e0b2fc2e01e4349a4521d3860
Kimberly Dijkmans <kim.14420@outlook.com>
2023-07-14 17:43:36 +0200

    added navigation handler and ECO Switch

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React from 'react'
 import backIcon from '../assets/Back-icon.svg'
-// import CustomSwitch from '../CustomSwitch';
-import { useNavigate  } from 'react-router-dom'
+import ECOSwitch from './ECOSwitch'
+import { useLocation, useNavigate } from "react-router-dom"
+
+// const NAV_TARGETS = {
+//     start: '/',
+//     home: '/home',
+//     trips: '/trips',
+//     // trip: '/trips/:id',
+//     // addTrip: '/add-trip',
+//     // editTrip: '/edit-trip/:id',
+//     rewards: '/rewards'
+// }
 
 export default function Header() {
-  const navigate = useNavigate()
+    const navigate = useNavigate()
+    const location = useLocation()
 
-  return (
-    <div className='header-container'>
-      <button className='back-btn' onClick={() => navigate(-1)}>
-        <img src={backIcon} alt="back button" />
-      </button>
+    // function handleNavigation() {
+    //     const navBackTarget = location.state?.navBackTarget || null
+    //     if (navBackTarget && Object.values(NAV_TARGETS).includes(navBackTarget)) {
+    //         console.log("Navigate to target")
+    //         navigate(navBackTarget)
+    //     } else {
+    //         console.log("Navigate to -1")
+    //         navigate(-1)
+    //     }
+    // }
 
-      <div className='switch-container'>
-        <p className='eco-mode'>ECO</p>
-        {/* <CustomSwitch /> */}
-      </div>
+    return (
+        <div className='header-container'>
+            <button className='back-btn' onClick={() => navigate(-1)}>
+                <img src={backIcon} alt="back button" />
+            </button>
 
-    </div>
-  )
+            <div className='switch-container'>
+                <p className='eco-mode'>ECO</p>
+                <ECOSwitch />
+            </div>
+        </div>
+    )
 }
