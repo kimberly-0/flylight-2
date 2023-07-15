46e03ad4a074cd25a1a7666b25283cb9adf2c266
Kimberly Dijkmans <kim.14420@outlook.com>
2023-07-15 16:07:22 +0200

    move weather and reward sections to own component

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,46 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { useAsync } from '../hooks/useAsync'
-import { getTrips } from '../services/userTrips'
 import TripsList from './TripsList'
-import WeatherItem from './WeatherItem'
-import RewardStatus from './RewardStatus'
+import WeatherList from './WeatherList'
+import RewardStatusList from './RewardStatusList'
 
 export default function HomePage({ userId }) {
-
-    const { loading, error, value: trips } = useAsync(() => getTrips({ userId }), [userId])
-
-    if (loading) return <h1>Loading</h1>
-
-    if (error) return <h1 className="error-msg">{error}</h1>
-
-    // console.log("Trips: " + JSON.stringify(trips))
-
     return (
         <div className="home">
 
-            <div className="home__weather">
-                <h2 className="home__weather__title">Weather for my next trip</h2>
-                <div className="home__weather__items">
-                    <WeatherItem />
-                    <WeatherItem />
-                    <WeatherItem />
-                    <WeatherItem />
-                    <WeatherItem />
-                </div>
-            </div>
+            <WeatherList userId={userId} />
 
             <TripsList userId={userId} shortList={true} />
 
-            <div className="home__my-rewards">
-                <div className="home__my-rewards__header">
-                    <h2 className="home__my-rewards__title">My rewards</h2>
-                    <Link to="/rewards" className="home__my-rewards__button--see-all button-turquoise button-turquoise--small">See all</Link>
-                </div>
-                <div className="home__my-rewards__items">
-                    <RewardStatus userId={userId} />
-                </div>
-            </div>
+            <RewardStatusList userId={userId} isRewardPage={false} />
 
         </div>
     )
